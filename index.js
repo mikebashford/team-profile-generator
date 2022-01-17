bb3989d961552628b898bc4d25feeef6014955f5
Michael Bashford <mikebashford@gmail.com>
2022-01-16 22:11:06 -0600

    Updated generated page to match video file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-const { write } = require('fs');
 const team = [];
 
 const promptManager = () =>
